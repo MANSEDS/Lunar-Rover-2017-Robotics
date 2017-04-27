a81fecee45c55fdebac0be07d4fb0ee7d1c42307
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-04-27 13:00:29 +0000

    Servo controller fixes.

--- Servo_controller.py
+++ Servo_controller.py
@@ -1,34 +1,36 @@
+
 # Controller for Servos in the Robotic Arm of the MANSEDS Lunar Rover
 
 # Import dependencies
 import RPi.GPIO as GPIO
-
+import time
 
 # Set GPIO pin configuration
 GPIO.setmode(GPIO.BOARD)
 
 
 # Declare servo pins
-servo_pins = []
+servo_pins = [12]
 GPIO.setup(servo_pins, GPIO.OUT)
 
 
 # Define servo instance
 def servo_instance(servo_number):
-    servo_1 = GPIO.PWM(servo_pins[servo_number], 50)
+    servo_1 = GPIO.PWM(12, 50)#servo_pins[servo_number], 50)
+    return servo_1
 
 
 # Define degree converter
-servo_duty_cycles = [[], [], [], [], []]
+servo_duty_cycles = [[2,12], [], [], [], []]
 
 
 def degree_converter(servo_number, degree, max_degree):
     duty_cycle_limits = servo_duty_cycles[servo_number]
-    duty_cycle_range = duty_cycle_limits[2] - duty_cycle_limits[1]
+    duty_cycle_range = 10#duty_cycle_limits[2] - duty_cycle_limits[1]
     if max_degree == 180:
-        desired_duty_cycle = degree * duty_cycle_range / 180 + duty_cycle_limits[1]
+        desired_duty_cycle = degree * duty_cycle_range / 180 + 2#duty_cycle_limits[1]
     elif max_degree ==360:
-        desired_duty_cycle = degree * duty_cycle_range / 180 + duty_cycle_limits[1]
+        desired_duty_cycle = degree * duty_cycle_range / 360 + 2#duty_cycle_limits[1]
     else:
         raise ValueError('Maximum degree not equal to 180 or 360')
     return desired_duty_cycle
@@ -36,9 +38,11 @@ def degree_converter(servo_number, degree, max_degree):
 
 # Define servo controller
 def servo_controller(servo_number, degree):
-    servo_instance(servo_number)
-    dc = degree_converter(servo_number,degree)
-    servo_instance.start(dc)
+    dc = degree_converter(servo_number,degree, 180)
+    servo_instance(servo_number).start(dc)
 
 
-
+while True:
+    for i in range(0,180):
+        #time.sleep(1)
+        servo_controller(1, i)
