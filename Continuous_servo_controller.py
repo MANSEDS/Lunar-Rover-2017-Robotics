e0ba4400ffa5e762645aa9f3231589d34f61fe25
era314 <ethanlramsay@gmail.com>
2017-10-19 17:16:06 +0000

    Got continuous servo running and calibrated, still need to fix zeroing issue

--- Continuous_servo_controller.py
+++ Continuous_servo_controller.py
@@ -20,6 +20,10 @@ logging.basicConfig(filename='servo.log', level=logging.WARNING)
 pi = 3.14159
 servo = 0
 arm_angle = 0 # degrees
+zero_motion_pl = 410
+compensation_factor = 1.027
+ccw_full_rot_time = 1.1 # s / 360 deg
+
 
 # GPIO setup function
 def GPIO_set(pin, dc):
@@ -40,19 +44,46 @@ pwm.set_pwm_freq(60)
 
 
 # Funcs
-def determine_zero_pl(pl_min, pl_max):
+def determine_zero_pl(pl_min, pl_max, channel):
     mid_range_pl = (pl_min + pl_max)/2
     initial_pl = mid_range_pl - 100
     pl = int(initial_pl)
     while pl < (mid_range_pl+100):
         pwm.set_pwm(channel, 0, pl)
         print(pl)
-        pl + 10
+        pl += 10
         time.sleep(1)
 
 
-def speed_loop():
-    pass
+def speed_loop(pl_min, pl_max, channel):
+    pwm.set_pwm(channel,0,410)
+    time.sleep(2)
+    pwm.set_pwm(channel,0,440)
+    time.sleep(1.1)
+    pwm.set_pwm(channel,0,410)
+    time.sleep(5)    
+    pwm.set_pwm(channel,0,220)
+    time.sleep(1.1297)
+    pwm.set_pwm(channel,0,410)
+    time.sleep(5)
+
+
+def rotational_positioning(dir_bool, angle, channel):
+    perc_full_rot = 100 * angle / 360
+    print(perc_full_rot)
+    if dir_bool:
+        rot_time = ccw_full_rot_time * perc_full_rot / 100
+        print(rot_time)
+        pwm.set_pwm(channel, 0, 440)
+        time.sleep(rot_time)
+        pwm.set_pwm(channel, 0, 410)
+        time.sleep(100000)
+    else:
+        rot_time = ccw_full_rot_time * compensation_factor * perc_full_rot / 100
+        pwm.set_pwm(channel, 0, 220)
+        time.sleep(rot.time)
+        pwm.set_pwm(channel, 0, 410)
+        time.sleep(1000000)
 
 
 if __name__ == "__main__":
@@ -61,7 +92,8 @@ if __name__ == "__main__":
 
         # Arguments
         parser = argparse.ArgumentParser()
-        parser.add_argument("a", help="Angle (Degrees)")
+        parser.add_argument("angle", help="Angle of turn (deg)")
+        parser.add_argument("direction", help="Direction of turn - True = CCW")
 
         # Arguments for direct GPIO control from Pi
         # parser.add_argument("dc_min", help="Minimum Duty Cycle")
@@ -75,7 +107,8 @@ if __name__ == "__main__":
 
         # Parse arguments
         args = parser.parse_args()
-        a = float(args.a)
+        angle = int(args.angle)
+        direction = args.direction
         # dc_min = float(args.dc_min)
         # dc_max = float(args.dc_max)
         # pin = int(args.pin)
@@ -87,4 +120,5 @@ if __name__ == "__main__":
         # Actuate servo
         # GPIO_set(pin, dc)
         # GPIO_clear()
-        pwm.set_pwm(channel, 0, pl)
+        
+        rotational_positioning(direction, angle, channel)
